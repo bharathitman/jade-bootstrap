7f74dc9d810a81b0eee5cae0a0ddc39c60514c6d
Bharath Bhandarkar <bharathitman@gmail.com>
2016-03-09 22:43:48 +0530

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+//Static Reload
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+            index: "index.html"
+        }
+    });
+    gulp.watch('index.html').on('change',browserSync.reload);
+    gulp.watch('**/*.html').on('change',browserSync.reload);
+    gulp.watch('**/*.css').on('change',browserSync.reload);
+    gulp.watch('**/*.js').on('change',browserSync.reload);
+});
