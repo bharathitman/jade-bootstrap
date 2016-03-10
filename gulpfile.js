41d0b4f7d1501dd65e8df4d8e878f94f3a323b55
bharathitman <bharathitman@gmail.com>
2016-03-10 08:21:13 +0530

    Trying to Put Minimum Width

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var browserSync = require('browser-sync').create();
 
 //Static Reload
 gulp.task('browser-sync', function () {
-    browserSync.init({
-        server: {
-            baseDir: "./",
-            index: "index.html"
-        }
-    });
-    gulp.watch('index.html').on('change',browserSync.reload);
-    gulp.watch('**/*.html').on('change',browserSync.reload);
-    gulp.watch('**/*.css').on('change',browserSync.reload);
-    gulp.watch('**/*.js').on('change',browserSync.reload);
+  browserSync.init({
+    server: {
+      baseDir: './',
+      index: 'index.html',
+    },
+  });
+  gulp.watch('index.html').on('change', browserSync.reload);
+  gulp.watch('**/*.html').on('change', browserSync.reload);
+  gulp.watch('**/*.css').on('change', browserSync.reload);
+  gulp.watch('**/*.js').on('change', browserSync.reload);
 });
